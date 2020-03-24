dd56b3bee062f7dbe1e0be1f09c2b138ac956aae
Edoardo Conti <edoardo.conti@gmail.com>
2020-03-24 16:17:26 -0700

    Get experiment config from banditapp

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,11 @@ import json
 import logging
 import math
 
+import requests
+
+BANDIT_APP_CREDS_PATH = "credentials/bandit_app_creds.json"
+BANDIT_APP_EXP_CONFIG_URL = "https://www.banditml.com/api/get_bandit_exp_config"
+
 
 def read_config(config_path):
     with open(config_path) as json_file:
@@ -34,3 +39,24 @@ def fancy_print(text: str, color="blue", size=60):
     border = "#" * header_len
     message = "#" * padding + " " + text + " " + "#" * padding
     print(f"{ansi_color}\n{border}\n{message}\n{border}\n{end_color}")
+
+
+def get_experiment_config_from_bandit_app(experiment_id):
+    with open(BANDIT_APP_CREDS_PATH) as json_file:
+        creds = json.load(json_file)
+
+    api_key = creds["api_key"]
+    params = {"experiment_id": experiment_id}
+    response = requests.get(
+        url=BANDIT_APP_EXP_CONFIG_URL,
+        params=params,
+        headers={"Authorization": f"ApiKey {api_key}"},
+    )
+
+    if response.status_code != 200:
+        raise Exception(
+            "Get experiment config from bandit app failed with code: "
+            f"{response.status_code}\n"
+        )
+
+    return response.json()
