faadc86c18c4673aa43df93a8b9078984ce80478
Fergal <fergaljamesoconnor@gmail.com>
2021-07-01 21:37:38 +0100

    Add bootstrap intervals to IPS method

--- ope/methods/inverse_propensity_scoring.py
+++ ope/methods/inverse_propensity_scoring.py
@@ -2,8 +2,20 @@ from typing import Callable, Dict
 
 import pandas as pd
 
+from ..sampling.bootstrap import evaluation_and_bootstrap_metrics
 
-def evaluate(df: pd.DataFrame, action_prob_function: Callable) -> Dict[str, float]:
+
+def evaluate(
+    df: pd.DataFrame, action_prob_function: Callable, num_bootstrap_samples: int = 0
+):
+    return evaluation_and_bootstrap_metrics(
+        df,
+        lambda dataframe: evaluate_raw(dataframe, action_prob_function),
+        num_bootstrap_samples=num_bootstrap_samples,
+    )
+
+
+def evaluate_raw(df: pd.DataFrame, action_prob_function: Callable) -> Dict[str, float]:
     """
     Inverse propensity scoring (IPS) tutorial:
     https://www.cs.cornell.edu/courses/cs7792/2016fa/lectures/03-counterfactualmodel_6up.pdf
